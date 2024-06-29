6592e1d54309a887753a89e09b4a28c33db6d4a7
Kunal-Singh18 <kunalsingh92724@gmail.com>
2024-06-29 14:17:12 +0530

    toggle led code

--- 0_LED_TOGGLE/Src/main.c
+++ 0_LED_TOGGLE/Src/main.c
@@ -5,33 +5,55 @@ refer nucleo guide for the same.
  */
 
 
-#define PERIPH_BASE               (0x40000000UL)
+#define PERIPH_BASE                (0x40000000UL)
 
-#define AHB1PERIPH_OFFSET         (0x00020000UL)
-#define AHB1PERIPH_BASE           (PERIPH_BASE + AHB1PERIPH_OFFSET)
+#define AHB1PERIPH_OFFSET          (0x00002000UL)
+#define AHB1PERIPH_BASE            (PERIPH_BASE + AHB1PERIPH_OFFSET)
 
-#define GPIOD_OFFSET              (0x0C00UL)
+#define GPIOD_OFFSET               (0x0C00UL)
 #define GPIOD_BASE                 (AHB1PERIPH_BASE + GPIOD_OFFSET)
 
 #define RCC_OFFSET                (0x3800UL)
 #define RCC_BASE                  (AHB1PERIPH_BASE + RCC_OFFSET)
 
+#define AHB1EN_R_OFFSET            (0x30UL)
+#define RCC_AHB1EN_R                   (*(volatile unsigned int *)(RCC_BASE + AHB1EN_R_OFFSET))
 
-#define AHB1EN_R_OFFSET            (0x10UL)
-#define AHB1EN_R                   (*(volatile unsigned int *)(RCC_BASE + AHB1EN_R_OFFSET)
-
-
-
-#define MODE_R_OFFSET                  (0x00UL)
-#define GPIOA_MODER_R                 (*(volatile unsigned int *)(GPIOD_BASE + MODE_R_OFFSET)
+#define MODE_R_OFFSET                 (0x00UL)
+#define GPIOD_MODER_R                 (*(volatile unsigned int *)(GPIOD_BASE + MODE_R_OFFSET))
 
 #define OD_R_OFFSET                 (0x14UL)
-#define GPIOS_OD_R                  (*(volatile unsigned int *)(GPIOD_BASE + OD_R_OFFSET)
+#define GPIOD_OD_R                  (*(volatile unsigned int *)(GPIOD_BASE + OD_R_OFFSET))
 
 
-#define PIN12                       (1U<<12)// used for adjusting value of pin12 in output data register.
+#define PIN12                        (1U<<12)// used for adjusting value of pin12 in output data register.
 #define LED_PIN                      PIN12
 
 
-#define GPIODEN                   (1U<<3)
-// in order to toggle the LED connected to PD12 we need to set the pin to output mode
+#define GPIODEN                    (1U<<3)
+
+
+int main(void)
+{
+	/* 1. enable clock access to GPIOD */
+	RCC_AHB1EN_R |=GPIODEN;
+
+	/* 2. set the pin12 as output pin */
+	GPIOD_MODER_R |= (1U<<24);
+	GPIOD_MODER_R &= ~(1U<<25);
+
+
+
+
+
+	while(1)
+	{
+		/*3. toggle the LED pin*/
+		GPIOD_OD_R |=LED_PIN;
+
+	}
+}
+
+
+
+
