274af8cc24bb77069a01bc28e610989234781046
Kunal-Singh18 <kunalsingh92724@gmail.com>
2024-07-25 21:37:09 +0530

    timer interrupt

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/fa/50ad609a9e4a001f102f9bfb1212d6dd
@@ -0,0 +1,16 @@
+/*
+ * timer.h
+ *
+ *  Created on: Jul 13, 2024
+ *      Author: kunal
+ */
+
+#ifndef TIMER_H_
+#define TIMER_H_
+
+#define SR_UIF  (1U<<0)
+
+void tim3_1hz_init(void);
+
+
+#endif /* TIMER_H_ */
---
+++ 20_timer_interrupt/Src/timer.c
@@ -0,0 +1,44 @@
+#include "stm32f4xx.h"
+
+#define TIM3EN         (1U<<1)
+#define CR1_CEN        (1U<<0)
+#define DIER_UIE       (1U<<0)
+
+
+void tim3_1hz_init(void)
+{
+	//enable clock access
+	RCC->APB1ENR = TIM3EN;
+
+	// set prescaler value
+	TIM3->PSC = 1600 -1;  //   16000000 / 1600 = 10000
+	// set auto reload value
+	TIM3->ARR = 10000 -1; // 10000/10000 = 1;
+	// clear counter
+	TIM3->CNT =0;
+	// enable timer
+	TIM3->CR1 =CR1_CEN;
+
+}
+void tim3_1hz_interrupt_init(void)
+{
+	//enable clock access
+	RCC->APB1ENR = TIM3EN;
+
+	// set prescaler value
+	TIM3->PSC = 1600 -1;  //   16000000 / 1600 = 10000
+	// set auto reload value
+	TIM3->ARR = 10000 -1; // 10000/10000 = 1;
+	// clear counter
+	TIM3->CNT =0;
+	// enable timer
+	TIM3->CR1 =CR1_CEN;
+
+	//enable timer interrupt
+	TIM3->DIER |=DIER_UIE;
+
+	//enable timer interrupt in NVIC
+	NVIC_EnableIRQ(TIM3_IRQn);
+
+}
+
